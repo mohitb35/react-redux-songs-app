e6432d2b0d322f4d0a07c7ebed088081c7464b73
Mohit Bajaj <mohit_b35@hotmail.com>
2020-04-27 17:33:26 +0530

    configure reducers 1. create songsReducer and selectedSongReducer 2. combine using combineReducers (imported from redux)

---
+++ src/reducers/index.js
@@ -0,0 +1,24 @@
+import { combineReducers } from 'redux';
+
+// Reducers
+const songsReducer = () => {
+	return [
+		{ title: 'Bohemian Rhapsody', duration: '3:45'},
+		{ title: 'Born To Be Wild', duration: '2:59'},
+		{ title: 'Nothing Else Matters', duration: '7:04'},
+		{ title: 'Ho Hey', duration: '2:49'}
+	];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+	if (action.type === 'SONG_SELECTED') {
+		return action.payload;
+	}
+
+	return selectedSong;
+}
+
+export default combineReducers({
+	songs: songsReducer,
+	selectedSong: selectedSongReducer
+});
